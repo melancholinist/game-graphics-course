384a62564d1d6cc46ec14f5c6df0e5f3c872f8a2
Sopita Daniels <sopitanamwan@gmail.com>
2018-10-09 01:23:34 +0300

    Merge branch 'example'

---
+++ demos/texturing.js
@@ -0,0 +1,325 @@
+// *********************************************************************************************************************
+// **                                                                                                                 **
+// **             Texturing example, Cube is mapped with 2D texture, skybox is mapped with a Cubemap                  **
+// **                                                                                                                 **
+// *********************************************************************************************************************
+
+// * Change textures
+// * Combine several textures in fragment shaders
+// * Distort UV coordinates
+// * Change texture filtering for pixel graphics
+// * Use wrapping modes for texture tiling
+
+
+let positions = new Float32Array([
+    // front
+    -0.5, 0.5, 0.5,
+    0.5, 0.5, 0.5,
+    0.5, -0.5, 0.5,
+    -0.5, -0.5, 0.5,
+
+    // back
+    -0.5, 0.5, -0.5,
+    0.5, 0.5, -0.5,
+    0.5, -0.5, -0.5,
+    -0.5, -0.5, -0.5,
+
+    //top
+    -0.5, 0.5, 0.5,
+    0.5, 0.5, 0.5,
+    0.5, 0.5, -0.5,
+    -0.5, 0.5, -0.5,
+
+    //bottom
+    -0.5, -0.5, 0.5,
+    0.5, -0.5, 0.5,
+    0.5, -0.5, -0.5,
+    -0.5, -0.5, -0.5,
+
+    //left
+    -0.5, -0.5, 0.5,
+    -0.5, 0.5, 0.5,
+    -0.5, 0.5, -0.5,
+    -0.5, -0.5, -0.5,
+
+    //right
+    0.5, -0.5, 0.5,
+    0.5, 0.5, 0.5,
+    0.5, 0.5, -0.5,
+    0.5, -0.5, -0.5,
+]);
+
+let normals = new Float32Array([
+    // front
+    0.0, 0.0, 1.0,
+    0.0, 0.0, 1.0,
+    0.0, 0.0, 1.0,
+    0.0, 0.0, 1.0,
+
+    // back
+    0.0, 0.0, -1.0,
+    0.0, 0.0, -1.0,
+    0.0, 0.0, -1.0,
+    0.0, 0.0, -1.0,
+
+    //top
+    0.0, 1.0, 0.0,
+    0.0, 1.0, 0.0,
+    0.0, 1.0, 0.0,
+    0.0, 1.0, 0.0,
+
+    //bottom
+    0.0, -1.0, 0.0,
+    0.0, -1.0, 0.0,
+    0.0, -1.0, 0.0,
+    0.0, -1.0, 0.0,
+
+    //left
+    -1.0, 0.0, 0.0,
+    -1.0, 0.0, 0.0,
+    -1.0, 0.0, 0.0,
+    -1.0, 0.0, 0.0,
+
+    //right
+    1.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+]);
+
+let uvs = new Float32Array([
+    // front
+    0.0, 1.0,
+    1.0, 1.0,
+    1.0, 0.0,
+    0.0, 0.0,
+
+    // back
+    0.0, 1.0,
+    1.0, 1.0,
+    1.0, 0.0,
+    0.0, 0.0,
+
+    //top
+    0.0, 1.0,
+    1.0, 1.0,
+    1.0, 0.0,
+    0.0, 0.0,
+
+    //bottom
+    0.0, 1.0,
+    1.0, 1.0,
+    1.0, 0.0,
+    0.0, 0.0,
+
+    //left
+    0.0, 1.0,
+    1.0, 1.0,
+    1.0, 0.0,
+    0.0, 0.0,
+
+    //right
+    0.0, 1.0,
+    1.0, 1.0,
+    1.0, 0.0,
+    0.0, 0.0,
+]);
+
+let triangles = new Uint16Array([
+    // front
+    2, 1, 0,
+    0, 3, 2,
+
+    // back
+    4, 5, 6,
+    6, 7, 4,
+
+    // top
+    8, 9, 10,
+    10, 11, 8,
+
+    // bottom
+    14, 13, 12,
+    12, 15, 14,
+
+    // left
+    16, 17, 18,
+    18, 19, 16,
+
+    // right
+    22, 21, 20,
+    20, 23, 22,
+]);
+
+let skyboxPositions = new Float32Array([
+    -1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    -1.0, -1.0, 1.0,
+    1.0, -1.0, 1.0
+]);
+
+let skyboxTriangles = new Uint16Array([
+    0, 2, 1,
+    2, 3, 1
+]);
+
+
+// language=GLSL
+let fragmentShader = `
+    #version 300 es
+    precision highp float;
+
+    uniform sampler2D tex;
+    uniform float time;
+
+    in vec2 v_uv;
+
+    out vec4 outColor;
+
+    void main()
+    {
+        // Combining 2 texture samples with multiplied uv coordinates
+        // Using time for second uv animation
+        outColor = texture(tex, (v_uv - 0.5) * 2.0)
+                   + texture(tex, (v_uv - 0.5) * 2.0 * sin(time * 3.46641));
+    }
+`;
+
+// language=GLSL
+let vertexShader = `
+    #version 300 es
+
+    uniform mat4 modelViewProjectionMatrix;
+
+    layout(location=0) in vec3 position;
+    layout(location=1) in vec3 normal;
+    layout(location=2) in vec2 uv;
+
+    out vec2 v_uv;
+
+    void main()
+    {
+        // Cube size is multiplied by 2
+        gl_Position = modelViewProjectionMatrix * vec4(position, 1.0) * 2.0;
+        v_uv = uv;
+    }
+`;
+
+
+// language=GLSL
+let skyboxFragmentShader = `
+    #version 300 es
+    precision mediump float;
+
+    uniform samplerCube cubemap;
+    uniform samplerCube cubemap2;
+    uniform mat4 viewProjectionInverse;
+    in vec4 v_position;
+
+    out vec4 outColor;
+
+    void main() {
+      vec4 t = viewProjectionInverse * v_position;
+
+      // Combining 2 cubemaps
+      outColor = texture(cubemap, normalize(t.xyz / t.w)) * texture(cubemap2, normalize(t.xyz / t.w));
+    }
+`;
+
+// language=GLSL
+let skyboxVertexShader = `
+    #version 300 es
+
+    layout(location=0) in vec4 position;
+    out vec4 v_position;
+
+    void main() {
+      v_position = position;
+      gl_Position = position;
+    }
+`;
+
+app.cullBackfaces();
+
+let program = app.createProgram(vertexShader.trim(), fragmentShader.trim());
+let skyboxProgram = app.createProgram(skyboxVertexShader.trim(), skyboxFragmentShader.trim());
+
+let vertexArray = app.createVertexArray()
+    .vertexAttributeBuffer(0, app.createVertexBuffer(PicoGL.FLOAT, 3, positions))
+    .vertexAttributeBuffer(1, app.createVertexBuffer(PicoGL.FLOAT, 3, normals))
+    .vertexAttributeBuffer(2, app.createVertexBuffer(PicoGL.FLOAT, 2, uvs))
+    .indexBuffer(app.createIndexBuffer(PicoGL.UNSIGNED_SHORT, 3, triangles));
+
+let skyboxArray = app.createVertexArray()
+    .vertexAttributeBuffer(0, app.createVertexBuffer(PicoGL.FLOAT, 3, skyboxPositions))
+    .indexBuffer(app.createIndexBuffer(PicoGL.UNSIGNED_SHORT, 3, skyboxTriangles));
+
+let projMatrix = mat4.create();
+let viewMatrix = mat4.create();
+let viewProjMatrix = mat4.create();
+let modelMatrix = mat4.create();
+let modelViewMatrix = mat4.create();
+let modelViewProjectionMatrix = mat4.create();
+let rotateXMatrix = mat4.create();
+let rotateZMatrix = mat4.create();
+let skyboxViewProjectionInverse = mat4.create();
+
+
+loadImages(["images/lemon2.jpg", "images/coolercubemap.jpg"], function (images) {
+    let drawCall = app.createDrawCall(program, vertexArray)
+        .texture("tex", app.createTexture2D(images[0], images[0].width, images[0].height, {flipY: true, magFilter: PicoGL.NEAREST, wrapT: PicoGL.MIRRORED_REPEAT, wrapS: PicoGL.MIRRORED_REPEAT}));
+
+    let skyboxDrawCall = app.createDrawCall(skyboxProgram, skyboxArray)
+        .texture("cubemap", app.createCubemap({cross: images[1]}))
+
+        // Added second cubemap with all 6 sides mapped to texture.jpg
+        .texture("cubemap2", app.createCubemap({
+            negX: images[0],
+            posX: images[0],
+            negY: images[0],
+            posY: images[0],
+            negZ: images[0],
+            posZ: images[0]
+        }));
+
+    let startTime = new Date().getTime() / 1000;
+
+
+    function draw() {
+        let time = new Date().getTime() / 1000 - startTime;
+
+        mat4.perspective(projMatrix, Math.PI / 1.4, app.width / app.height, 0.1, 100.0);
+        let camPos = vec3.rotateY(vec3.create(), vec3.fromValues(0, -1, 1), vec3.fromValues(0, 0, 0), time);
+        mat4.lookAt(viewMatrix, camPos, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
+        mat4.multiply(viewProjMatrix, projMatrix, viewMatrix);
+
+        mat4.fromXRotation(rotateXMatrix, time * 0.1136 - Math.PI / 2);
+        mat4.fromZRotation(rotateZMatrix, -time * 2);
+        mat4.multiply(modelMatrix, rotateXMatrix, rotateZMatrix);
+
+        mat4.multiply(modelViewMatrix, viewMatrix, modelMatrix);
+        mat4.multiply(modelViewProjectionMatrix, viewProjMatrix, modelMatrix);
+
+        let skyboxView = mat4.clone(viewMatrix);
+        mat4.setTranslation(skyboxView, vec3.fromValues(0, 0, 0));
+        let skyboxViewProjectionMatrix = mat4.create();
+        mat4.mul(skyboxViewProjectionMatrix, projMatrix, skyboxView);
+        mat4.invert(skyboxViewProjectionInverse, skyboxViewProjectionMatrix);
+
+        app.clear();
+
+        app.noDepthTest();
+        skyboxDrawCall.uniform("viewProjectionInverse", skyboxViewProjectionInverse);
+        skyboxDrawCall.draw();
+
+        app.depthTest();
+        drawCall.uniform("modelViewProjectionMatrix", modelViewProjectionMatrix);
+        // passing time value for uv animation
+        drawCall.uniform("time", time);
+        drawCall.draw();
+
+        requestAnimationFrame(draw);
+    }
+
+    requestAnimationFrame(draw);
+});
